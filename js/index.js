32e09cac1b1fb7c2d13382c2aae3776ae92e2ab9
Maki <maki.matsumoto@g.dreamcareer.co.jp>
2024-11-19 17:00:31 +0900

    HTMLの作成

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// const boxes = document.querySelectorAll('.pageTransition__link');
+
+// boxes.forEach(box => {
+//     const container = box.querySelector('.pageTransition__container');
+//     box.addEventListener('mouseover', () => {
+//         box.classList.add('hovered');
+//         if (container) {
+//             container.classList.add('hovered');
+//         }
+//     });
+
+// });
