02f04c8dea5c93cfc17e935e628d19a8cc063ecb
Maki <maki.matsumoto@g.dreamcareer.co.jp>
2024-12-18 11:27:44 +0900

    SP版の修正途中

--- Sample/js/index.js
+++ Sample/js/index.js
@@ -10,3 +10,22 @@ boxes.forEach(box => {
     });
 
 });
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const links = document.querySelectorAll('.pageTransition__link');
+//     links.forEach(link => {
+//         let isExpanded = false; // タッチ状態を追跡
+
+//         link.addEventListener('touchstart', (e) => {
+//             if (!isExpanded) {
+//                 e.preventDefault(); // 初回タッチでリンク移動を無効化
+//                 link.classList.add('hovered');
+//                 link.querySelector('.pageTransition__container').classList.add('hovered');
+//                 isExpanded = true; // 次のタッチでリンク移動を許可
+//             } else {
+//                 window.location.href = link.href; // リンク先に移動
+//             }
+//         });
+//     });
+// });
+
